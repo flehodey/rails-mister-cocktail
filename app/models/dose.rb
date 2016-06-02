390f0b3bf408a4f3c66c5f3248e10dad2a429569
felix le hodey <felixlehodey@gmail.com>
2016-06-02 12:34:09 +0200

    validations done

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,8 @@
 class Dose < ActiveRecord::Base
   belongs_to :ingredient
   belongs_to :cocktail
+validates :description, presence: true
+validates :cocktail, presence: true
+validates :ingredient, presence: true
+validates :cocktail, uniqueness: { scope: :ingredient }
 end
