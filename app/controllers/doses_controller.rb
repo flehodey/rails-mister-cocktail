137514c7d0b42bc2bfe581f6bee434716838649a
felix le hodey <felixlehodey@gmail.com>
2016-06-03 10:51:52 +0200

    delete function added

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -19,10 +19,11 @@ class DosesController < ApplicationController
   end
 
   def destroy
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.find(params[:id])
     @dose.destroy
 
-    redirect_to cocktail_path
+    redirect_to cocktail_path(@cocktail)
   end
   private
 
